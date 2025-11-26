2b3c3cfd97d0d32ced463772b46a60e8b4bb8138
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2025-11-26 18:54:53 +0200

    deleted dist

--- src/dist/py/add_contact.py
+++
@@ -1,8 +0,0 @@
-def add_contact(args, contacts):
-    if len(args) != 2:
-        return 'Please provide both name and phone number.'
-    
-    name, phone = args
-    contacts[name] = phone
-
-    return "Contact added."
--- src/dist/py/parse_input_(2).py
+++
@@ -1,5 +0,0 @@
-def parse_input(user_input: str):
-    cmd, *args = user_input.split()
-    cmd = cmd.strip().lower()
-    
-    return cmd, *args
